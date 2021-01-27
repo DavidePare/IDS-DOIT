a584dd7645d3db0a655c2ab744ca6d191c0ac031
loremorla <47327850+loremorla@users.noreply.github.com>
2021-01-27 11:32:49 +0100

    BackUp progetto prima iterazione

--- doit/src/main/java/it/unicam/ids/doit/dao/ProgettistaRepository.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.ids.doit.dao;
-
-import it.unicam.ids.doit.service.ProgettistaService;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgettistaRepository extends JpaRepository<ProgettistaService,Long> { // decidere se Long o Integer
-}
