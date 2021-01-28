ccbeb7bf399df9b4c983c16636bb8d0571d8de5b
loremorla <47327850+loremorla@users.noreply.github.com>
2021-01-28 12:01:41 +0100

    rimosso DOIT-PrimaIterazione perchè dava fastidio

--- DOIT_PrimaIterazione/src/main/java/IState.java
+++
@@ -1,8 +0,0 @@
-public interface IState {
-    void confirm();
-    void decline();
-    void incrementAmount(double amount);
-    void decrementAmount(double amount);
-    void addCandidato(Progettista progettista);
-    void removeCandidato(Progettista progettista);
-}
