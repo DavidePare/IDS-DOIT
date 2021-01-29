64a208945194e47327916f1f44519191276311cb
loremorla <47327850+loremorla@users.noreply.github.com>
2021-01-29 16:05:47 +0100

    delete the old project because there were problem

--- doit/src/main/java/it/unicam/ids/doit/dao/CurriculumRepository.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.ids.doit.dao;
-
-import it.unicam.ids.doit.entity.Curriculum;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CurriculumRepository extends JpaRepository<Curriculum,Long> {
-}
--- doit/src/main/java/it/unicam/ids/doit/dao/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package it.unicam.ids.doit.dao;
-
-import it.unicam.ids.doit.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends JpaRepository<Team,Long> {
-}
